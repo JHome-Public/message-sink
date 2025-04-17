96e358e7cc0e951f04aa59233843cb9adf39275b
given02 <votmdnjem_id@naver.com>
2025-04-17 14:57:57 +0900

    Updated on 2025-04-17

---
+++ src/main/java/com/jhome/message/MessageSinkApplication.java
@@ -0,0 +1,13 @@
+package com.jhome.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageSinkApplication.class, args);
+    }
+
+}
